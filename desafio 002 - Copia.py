b99ad17af77dc1bbb6cc0c717981ed098a0eb449
Willrodagu <liamred@outlook.com>
2021-10-28 14:12:39 -0300

    Create desafio 002 - Copia.py

---
+++ desafio 002 - Copia.py
@@ -0,0 +1,4 @@
+#Faça um programa que pergunte o nome da pessoa que esta acessando e de as boas vindas seguido do nome da pessoa.
+
+Nome = input('Digite o seu nome: ')
+print('Seja bem vindo(a) {}!'.format(Nome))
